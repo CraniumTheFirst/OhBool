26eed621edb23014428a65ddaee6984e44090d28
Romain Gehrig <romain.gehrig@gmail.com>
2015-05-07 15:35:38 +0200

    Miscellaneous utils (gray code sorting, hamming distances)

--- src/OhBool/Utils.hs
+++ src/OhBool/Utils.hs
@@ -1,5 +1,23 @@
 module OhBool.Utils where
 
+import Data.Bits (Bits, shiftR, xor, popCount)
+import Data.List (sortBy)
+import Data.Function (on)
+
+import Codec.Binary.Gray.Bits (binary)
+
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead (x:_) = Just x
+
+grayify :: (Ord a, Eq a) => [a] -> [a]
+grayify xs = map snd $ sortBy (compare `on` fst) $ zipWith (\n e -> (binary n,e)) ([0..] :: [Int]) xs
+
+hammingDistance :: (Eq a) => [a] -> [a] -> Int
+hammingDistance xs ys = length $ filter id $ zipWith (/=) xs ys
+
+numHammingDistance :: Bits a => a -> a -> Int
+numHammingDistance x y = popCount (x `xor` y)
+
+pairs :: Int -> [a] -> [(a,a)]
+pairs n xs = zip xs (drop n xs)
