cdc77193e45ef5bc1a78a8d8d96f3bf027d679d1
Romain Gehrig <romain.gehrig@gmail.com>
2015-09-02 08:39:19 +0200

    Pretty printing of expressions

--- src/OhBool/Common.hs
+++ src/OhBool/Common.hs
@@ -2,18 +2,31 @@
 module OhBool.Common where
 
 import qualified Data.Map as M
+import Data.Ord (compare,comparing)
+import Data.List (maximumBy, intercalate)
+import Data.Function (on)
 
 data BinaryOperator = Or
                     | And
                     | Xor
-                    deriving (Show, Eq)
+                    deriving (Eq)
 
-type VarValue = Bool
-newtype Var = Var Char deriving (Show,Ord,Eq)
+instance Show BinaryOperator where
+  show Or = "⋁"
+  show And = "⋀"
+  show Xor = "⊕"
+
+type VarValue = Bool --T | F | Und deriving (Show, Eq)
+newtype Var = Var Char deriving (Ord,Eq)
+
+instance Show Var where
+  show (Var c) = c:""
+
 type Vars = M.Map Var VarValue
 
 data TruthTable = TruthTable { expression :: Expression
-                             , evaluation :: M.Map Vars Bool } deriving Eq
+                             , evaluation :: M.Map Vars VarValue } deriving Eq
+
 
 instance Show TruthTable where
   show (TruthTable expr xs) = "Truth table for " ++ show expr ++ "\n" ++ (show xs)
@@ -26,4 +39,35 @@ data Expression = BinaryExpression BinaryOperator Expression Expression
                 | BoolChain BinaryOperator [Expression]
                 | BoolValue Bool
                 | Variable Var
-                deriving (Show, Eq)
+                deriving (Eq)
+
+precedenceOp :: BinaryOperator -> Int
+precedenceOp Or = 2
+precedenceOp Xor = 2
+precedenceOp And = 3
+
+precedence :: Expression -> Int
+precedence (BoolValue _) = 5
+precedence (Variable _) = 5
+precedence (Not _) = 4
+precedence (BinaryExpression op _ _) = precedenceOp op
+precedence (BoolChain op _) = precedenceOp op
+
+needParentheses :: Expression -> Expression -> Bool
+needParentheses parent expr = precedence parent > precedence expr
+
+addParentheses :: String -> String
+addParentheses s = "(" ++ s ++ ")"
+
+exprPrettyPrint :: Expression -> String
+exprPrettyPrint (Variable x) = show x
+exprPrettyPrint expr@(Not ex) = "~" ++ ex' ++ ""
+  where ex' = (if needParentheses expr ex then addParentheses else id) $ show ex
+exprPrettyPrint (BoolValue b) = show b
+exprPrettyPrint expr@(BinaryExpression op exp1 exp2) = parenthify exp1 ++ " " ++ show op ++ " " ++ parenthify exp2
+  where parenthify e = (if needParentheses expr e then addParentheses else id) $ show e
+exprPrettyPrint expr@(BoolChain op exprs) = intercalate (" " ++ show op ++ " ") $ map parenthify $ exprs
+  where parenthify e = (if needParentheses expr e then addParentheses else id) $ show e
+
+instance Show Expression where
+  show = exprPrettyPrint
